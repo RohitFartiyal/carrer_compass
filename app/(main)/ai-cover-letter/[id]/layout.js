3014b30a8b63542b9d40c9ecda4ab2ea949be89c
RohitFartiyal <fartiyalrohit0@gmail.com>
2025-03-29 17:15:58 +0530

    preparing for deployment

---
+++ app/(main)/ai-cover-letter/[id]/layout.js
@@ -0,0 +1,16 @@
+"use client"
+
+import { Suspense } from "react";
+import { BarLoader } from "react-spinners";
+
+export default function Layout({ children }) {
+  return (
+    <div>
+      <Suspense
+        fallback={<BarLoader className="mt-4" width={"100%"} color="gray" />}
+      >
+        {children}
+      </Suspense>
+    </div>
+  );
+}
